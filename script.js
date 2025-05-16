328d603b4f845c9054ea43d4de05a3d25ec03eb0
keerthana <keerthanaeswaran21@gmail.com>
2025-05-16 16:00:41 +0530

    java script add

---
+++ script.js
@@ -0,0 +1,72 @@
+var a=10;
+var b=30;
+
+
+console.log(a)
+console.log(b)
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+
+function add(){
+    var a=10;
+    var b=30;
+    console.log(a+b)
+}
+add()
+function add(a,b){
+
+    console.log("a+b",a+b)
+
+}
+add(20,30)
+add(50,30)
+add(10,30)
+
+function sub(){
+    var a=10;
+    var b=30;
+    console.log(a-b)
+}
+sub()
+function sub(a,b){
+
+    console.log("a-b",a-b)
+
+}
+sub(20,30)
+sub(50,30)
+sub(10,30)
+
+function mul(){
+    var a=10;
+    var b=30;
+    console.log(a*b)
+}
+mul()
+function mul(a,b){
+
+    console.log("a*b",a*b)
+
+}
+mul(20,30)
+mul(50,30)
+mul(10,30)
+
+function div(){
+    var a=10;
+    var b=30;
+    console.log(a/b)
+}
+div()
+function div(a,b){
+
+    console.log("a/b",a,b)
+
+}
+div(20,30)
+div(50,30)
+div(10,30)
